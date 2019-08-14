d278cced0e394a8b50854c02fe596fd5fedda161
Hunter <hunterliu1003@gmail.com>
2019-08-14 15:43:56 +0800

    update

---
+++ src/components/BetChipSVGFunc.vue
@@ -0,0 +1,191 @@
+<template functional>
+  <svg
+    :width="props.width"
+    :height="props.height"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  >
+    <!-- <g> -->
+    <mask :id="`ellipseOuterMask${props.maskId}`">
+      <rect
+        :x="-props.padding"
+        :y="
+          props.offsetY + props.height / 2 - (props.height / 2 + props.padding)
+        "
+        :width="props.width + props.padding * 2"
+        :height="props.height + props.padding * 2"
+        fill="#fff"
+      />
+      <ellipse
+        class="ellipse-outer-mask"
+        :cx="props.width / 2"
+        :cy="props.offsetY + props.height / 2"
+        :rx="props.width / 2 + props.padding"
+        :ry="props.height / 2 + props.padding"
+        fill="#000"
+      />
+    </mask>
+    <ellipse
+      class="ellipse-outer"
+      :cx="props.width / 2"
+      :cy="props.offsetY + props.height / 2"
+      :rx="props.width / 2 + props.padding"
+      :ry="props.height / 2 + props.padding"
+      fill="#D3B27A"
+      :mask="`url(#ellipseOuterMask${props.maskId})`"
+    />
+
+    <mask :id="`ellipseInnerMask${props.maskId}`">
+      <rect
+        :x="-props.padding"
+        :y="
+          props.offsetY + props.height / 2 - (props.height / 2 + props.padding)
+        "
+        :width="props.width + props.padding * 2"
+        :height="props.height + props.padding * 2"
+        fill="#fff"
+      />
+      <ellipse
+        class="ellipse-inner-mask"
+        :cx="props.width / 2"
+        :cy="props.offsetY + props.height / 2"
+        :rx="props.width / 2 + props.padding"
+        :ry="props.height / 2 + props.padding"
+        fill="#000"
+      />
+    </mask>
+    <ellipse
+      class="ellipse-inner"
+      :cx="props.width / 2"
+      :cy="props.offsetY + props.height / 2"
+      :rx="props.width / 2 + props.padding"
+      :ry="props.height / 2 + props.padding"
+      fill="#FFEBC7"
+      :mask="`url(#ellipseInnerMask${props.maskId})`"
+    />
+    <!-- </g> -->
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      required: true
+    },
+    height: {
+      required: true
+    },
+    padding: {
+      required: true
+    },
+    offsetY: {
+      required: true
+    },
+    maskId: {
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.ellipse-outer {
+  transform-box: fill-box;
+  animation: scale-outer 6s linear infinite;
+  transform-origin: 50% 50%;
+  transform: scale(0.35);
+}
+.ellipse-outer-mask {
+  transform-box: fill-box;
+  animation: scale-outer-mask 6s linear infinite;
+  transform-origin: 50% 50%;
+  transform: scale(0.35);
+  will-change: transform, opacity;
+}
+@keyframes scale-outer {
+  0% {
+    transform: scale(0.35);
+    opacity: 1;
+  }
+  8% {
+    transform: scale(1);
+    opacity: 0;
+  }
+  99% {
+    transform: scale(1);
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+}
+@keyframes scale-outer-mask {
+  0% {
+    transform: scale(0.35);
+  }
+  8% {
+    transform: scale(1);
+  }
+  99% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+.ellipse-inner {
+  transform-box: fill-box;
+  animation: scale-inner 6s linear infinite;
+  transform-origin: 50% 50%;
+  transform: scale(0.35);
+  will-change: transform, opacity;
+}
+.ellipse-inner-mask {
+  transform-box: fill-box;
+  animation: scale-inner-mask 6s linear infinite;
+  transform-origin: 50% 50%;
+  transform: scale(0.35);
+}
+@keyframes scale-inner {
+  0% {
+    transform: scale(0.35);
+    opacity: 1;
+  }
+  4% {
+    transform: scale(0.35);
+    opacity: 1;
+  }
+  12% {
+    transform: scale(1);
+    opacity: 0;
+  }
+  99% {
+    transform: scale(1);
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+}
+@keyframes scale-inner-mask {
+  0% {
+    transform: scale(0.35);
+  }
+  4% {
+    transform: scale(0.35);
+  }
+  12% {
+    transform: scale(1);
+  }
+  99% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,50 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <button @click="setAll" style="display: block; margin: 20px auto 10px;">
+      all count
+    </button>
+    <input
+      type="text"
+      v-model="defaultCount"
+      style="display: block; margin: 0 auto 20px;"
+    />
     <div style="display: flex; justify-content: center;">
       <div>
         borderCount {{ borderCount }}
-        <button @click="setBorder">border version</button>
+        <button @click="start('borderCount')">border version</button>
         <BetChipBorder v-for="i in borderCount" :key="i" />
       </div>
       <div style="width: 50px;"></div>
       <div>
         ellipseCount {{ ellipseCount }}
-        <button @click="setEllipse">ellipse version</button>
+        <button @click="start('ellipseCount')">ellipse version</button>
         <BetChipEllipse v-for="i in ellipseCount" :key="i" />
       </div>
       <div style="width: 50px;"></div>
       <div>
         svgCount {{ svgCount }}
-        <button @click="setSVG">svg version</button>
+        <button @click="start('svgCount')">svg version</button>
         <div v-for="i in svgCount" :key="i" style="padding: 15px;">
           <BetChipSVG
             :width="80"
             :height="36"
             :padding="0"
             :maskId="`betChipSVG${i}`"
+            :offsetY="0"
+          />
+        </div>
+      </div>
+      <div style="width: 50px;"></div>
+      <div>
+        svgFuncCount {{ svgFuncCount }}
+        <button @click="start('svgFuncCount')">svgFunc version</button>
+        <div v-for="i in svgFuncCount" :key="i" style="padding: 15px;">
+          <BetChipSVGFunc
+            :width="80"
+            :height="36"
+            :padding="0"
+            :maskId="`betChipSVGFunc${i}`"
             :offsetY="0"
           />
         </div>
@@ -35,6 +57,7 @@
 import BetChipBorder from "@/components/BetChipBorder.vue";
 import BetChipEllipse from "@/components/BetChipEllipse.vue";
 import BetChipSVG from "@/components/BetChipSVG.vue";
+import BetChipSVGFunc from "@/components/BetChipSVGFunc.vue";
 import { setInterval, clearInterval } from "timers";
 
 export default {
@@ -42,60 +65,56 @@ export default {
   components: {
     BetChipBorder,
     BetChipEllipse,
-    BetChipSVG
+    BetChipSVG,
+    BetChipSVGFunc
   },
   data: () => ({
     borderCount: 0,
     ellipseCount: 0,
     svgCount: 0,
-    borderInterval: 0,
-    ellipseInterval: 0,
-    svgInterval: 0
+    svgFuncCount: 0,
+    allCount: 0,
+    allInterval: 0,
+    defaultCount: 100
   }),
-  // mounted() {
-  //   this.intervalId = setInterval(() => {
-  //     this.borderCount += 1;
-  //   }, 40);
-  // },
   methods: {
+    setAll() {
+      this.allInterval = setInterval(() => {
+        if (this.allCount === +this.defaultCount) {
+          clearInterval(this.allInterval);
+        } else {
+          this.allCount += 1;
+          this.borderCount = this.allCount;
+          this.ellipseCount = this.allCount;
+          this.svgCount = this.allCount;
+          this.svgFuncCount = this.allCount;
+        }
+      }, 40);
+    },
+    start(anim) {
+      this.reset();
+      this.allInterval = setInterval(() => {
+        if (this.allCount === +this.defaultCount) {
+          clearInterval(this.allInterval);
+        } else {
+          this.allCount += 1;
+          this[anim] = this.allCount;
+        }
+      }, 40);
+    },
     reset() {
-      clearInterval(this.borderInterval);
-      clearInterval(this.ellipseInterval);
-      clearInterval(this.svgInterval);
+      clearInterval(this.allInterval);
       this.borderCount = 0;
       this.ellipseCount = 0;
       this.svgCount = 0;
-    },
-    setBorder() {
-      this.reset();
-      this.borderInterval = setInterval(() => {
-        if (this.borderCount === 500) {
-          clearInterval(this.borderInterval);
-        } else {
-          this.borderCount += 1;
-        }
-      }, 40);
-    },
-    setEllipse() {
-      this.reset();
-      this.ellipseInterval = setInterval(() => {
-        if (this.ellipseCount === 500) {
-          clearInterval(this.ellipseInterval);
-        } else {
-          this.ellipseCount += 1;
-        }
-      }, 40);
-    },
-    setSVG() {
-      this.reset();
-      this.svgInterval = setInterval(() => {
-        if (this.svgCount === 500) {
-          clearInterval(this.svgInterval);
-        } else {
-          this.svgCount += 1;
-        }
-      }, 40);
+      this.svgCountFunc = 0;
+      this.allCount = 0;
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+}
+</style>
